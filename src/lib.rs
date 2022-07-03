852ba7cdb5c64db65dd6aa2a8c7c53955d3f232a
Andra Antariksa <andra.antariksa@gmail.com>
2022-07-03 15:46:46 +0700

    Fix the WGSL breaking change and NDK glue version mismatch

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::borrow::Cow;
+use android_logger::Config;
+use log::{debug, Level, log};
 use wgpu::{ColorTargetState, ColorWrites, PresentMode, SurfaceConfiguration, TextureViewDescriptor};
 use winit::{
     event::{Event, WindowEvent},
@@ -7,21 +9,21 @@ use winit::{
 };
 
 const SHADER: &'static str = r#"
-[[stage(vertex)]]
-fn vs_main([[builtin(vertex_index)]] in_vertex_index: u32) -> [[builtin(position)]] vec4<f32> {
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
     let x = f32(i32(in_vertex_index) - 1);
     let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
     return vec4<f32>(x, y, 0.0, 1.0);
 }
 
-[[stage(fragment)]]
-fn fs_main() -> [[location(0)]] vec4<f32> {
+@fragment
+fn fs_main() -> @location(0) vec4<f32> {
     return vec4<f32>(1.0, 0.0, 0.0, 1.0);
 }
 "#;
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
-    let instance = wgpu::Instance::new(wgpu::Backends::GL);
+    let instance = wgpu::Instance::new(wgpu::Backends::VULKAN);
     let adapter = instance
         .request_adapter(&wgpu::RequestAdapterOptions {
             power_preference: wgpu::PowerPreference::default(),
@@ -37,7 +39,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
             &wgpu::DeviceDescriptor {
                 label: None,
                 features: wgpu::Features::empty(),
-                limits: wgpu::Limits::default(),
+                limits: wgpu::Limits::downlevel_defaults(),
             },
             None,
         )
@@ -47,7 +49,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     // Load the shaders from disk
     let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
-        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(SHADER)),
+        source: wgpu::ShaderSource::Wgsl(SHADER.into()),
     });
 
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
@@ -88,7 +90,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         format: swapchain_format,
         width: window_size.width,
         height: window_size.height,
-        present_mode: PresentMode::Mailbox,
+        present_mode: PresentMode::Fifo,
     };
 
     let mut surface = None;
@@ -96,66 +98,77 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     let start = std::time::Instant::now();
 
     event_loop.run(move |event, _, control_flow| {
-        let _ = (&instance, &adapter, &shader, &pipeline_layout);
-
         *control_flow = ControlFlow::Poll;
         match event {
             Event::WindowEvent {
                 event: WindowEvent::Resized(size),
                 ..
             } => {
-                println!("resized");
+                debug!("resized");
+
                 surface_configuration.width = size.width;
                 surface_configuration.height = size.height;
             }
             Event::Resumed => {
-                println!("resume");
+                debug!("resumed");
+
                 surface = Some(unsafe { instance.create_surface(&window) });
                 surface.as_ref().unwrap().configure(&device, &surface_configuration);
-                println!("surface: {:?}", surface);
+                debug!("surface: {:?}", surface);
             }
             Event::Suspended => {
-                println!("suspend");
+                debug!("suspended");
+
                 surface.take();
             }
             Event::MainEventsCleared => {
-                let render_texture = surface.as_ref().unwrap().get_current_texture().unwrap();
-                let render_texture_view = render_texture.texture.create_view(&TextureViewDescriptor::default());
+                debug!("main events cleared");
 
-                let mut encoder = device
-                    .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-                {
-                    let t = (start.elapsed().as_secs_f64() / 10.0).sin();
-                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                        label: None,
-                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                            view: &render_texture_view,
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(wgpu::Color {
-                                    r: 0.0,
-                                    g: t,
-                                    b: 1.0 - t,
-                                    a: 1.0,
-                                }),
-                                store: true,
-                            },
-                        })],
-                        depth_stencil_attachment: None,
-                    });
-                    rpass.set_pipeline(&render_pipeline);
-                    rpass.draw(0..3, 0..1);
+                if let Some(_surface) = &surface {
+                    let render_texture = _surface.get_current_texture().unwrap();
+                    let render_texture_view = render_texture.texture.create_view(&TextureViewDescriptor::default());
+
+                    let mut encoder = device
+                        .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+                    {
+                        let t = (start.elapsed().as_secs_f64() / 10.0).sin();
+                        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                            label: None,
+                            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                                view: &render_texture_view,
+                                resolve_target: None,
+                                ops: wgpu::Operations {
+                                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                                        r: 0.0,
+                                        g: t,
+                                        b: 1.0 - t,
+                                        a: 1.0,
+                                    }),
+                                    store: true,
+                                },
+                            })],
+                            depth_stencil_attachment: None,
+                        });
+                        rpass.set_pipeline(&render_pipeline);
+                        rpass.draw(0..3, 0..1);
+                    }
+
+                    queue.submit(Some(encoder.finish()));
+
+                    render_texture.present();
                 }
-
-                queue.submit(Some(encoder.finish()));
-
-                render_texture.present();
             }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
+            } => {
+                debug!("quit");
+
+                *control_flow = ControlFlow::Exit
+            }
+            e => {
+                debug!("other event {:?}", e);
+            }
         }
     });
 }
@@ -165,9 +178,11 @@ target_os = "android",
 ndk_glue::main(backtrace = "on", logger(level = "debug", tag = "wgpu"))
 )]
 fn main() {
-    log::info!("start");
+    android_logger::init_once(
+        Config::default().with_min_level(Level::Trace),
+    );
     let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(&event_loop).unwrap();
+    let window = Window::new(&event_loop).unwrap();
 
     pollster::block_on(run(event_loop, window));
 }
